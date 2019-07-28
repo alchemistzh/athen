9f726cfeb55f3af708273d402646d40446d63fba
bluecover <gurity@163.com>
2019-07-29 00:11:15 +0800

    Scrapy tutorial

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -27,6 +27,7 @@ ROBOTSTXT_OBEY = True
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
+#RANDOMIZE_DOWNLOAD_DELAY = True
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16

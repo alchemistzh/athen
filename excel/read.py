b584e8f97857ac6c46af33a40e79e69dc131b614
zh <zh@xrc-inc.com>
2019-07-31 20:26:46 +0800

    Open xls file with rb

--- excel/read.py
+++ excel/read.py
@@ -2,8 +2,11 @@ __author__ = 'zh'
 # -*- coding: UTF-8 -*-
 
 import xlrd
+import os
 
-book = xlrd.open_workbook(file_contents=open(u"test.xls").read())
+print(os.getcwd())
+
+book = xlrd.open_workbook(file_contents=open(u'excel/test.xls', 'rb').read())
 print("The number of worksheets is", book.nsheets)
 print("Worksheet name(s):", book.sheet_names())
 sh = book.sheet_by_index(0)

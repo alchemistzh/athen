832d8c8f3ef2ab846101736a3f2e2439daa8a9e0
zh <zh@xrc-inc.com>
2019-07-31 20:17:30 +0800

    Sample codes to read excel

---
+++ excel/read.py
@@ -0,0 +1,14 @@
+__author__ = 'zh'
+# -*- coding: UTF-8 -*-
+
+import xlrd
+
+book = xlrd.open_workbook(file_contents=open(u"test.xls").read())
+print("The number of worksheets is", book.nsheets)
+print("Worksheet name(s):", book.sheet_names())
+sh = book.sheet_by_index(0)
+print(sh.name, sh.nrows, sh.ncols)
+for rx in range(1, sh.nrows):
+    row = sh.row(rx)
+    if row[1].value != u'\u5df2\u5904\u7406':  # 已处理
+        print(rx, row[0].value)

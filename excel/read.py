c29f9c68bfa22aa9bba63a846505c350a848d115
zh <zh@xrc-inc.com>
2019-08-01 16:24:52 +0800

    Try reading excel

--- excel/read.py
+++ excel/read.py
@@ -2,16 +2,12 @@ __author__ = 'zh'
 # -*- coding: UTF-8 -*-
 
 import xlrd
-import os
 
-print(os.getcwd())
-
-book = xlrd.open_workbook(file_contents=open(u'excel/test.xls', 'rb').read())
-print("The number of worksheets is", book.nsheets)
-print("Worksheet name(s):", book.sheet_names())
-sh = book.sheet_by_index(0)
-print(sh.name, sh.nrows, sh.ncols)
-for rx in range(1, sh.nrows):
-    row = sh.row(rx)
-    if row[1].value != u'\u5df2\u5904\u7406':  # 已处理
-        print(rx, row[0].value)
+sz = xlrd.open_workbook(file_contents=open(u'data/sz.xlsx', 'rb').read())
+print("Number of worksheets:", sz.nsheets)
+print("Worksheet name(s):", sz.sheet_names())
+st = sz.sheet_by_index(0)
+print(st.name, "rows:", st.nrows, "cols", st.ncols)
+for i in range(1, 10):
+    row = st.row(i)
+    print(row[0].value, row[1].value)

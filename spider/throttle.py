b8f2032c8bcdf37e6ece7fa22f4c08680d163c95
zh <zh@xrc-inc.com>
2019-08-06 17:33:29 +0800

    Add comments

--- spider/throttle.py
+++ spider/throttle.py
@@ -1,5 +1,8 @@
 from ratelimit import limits, sleep_and_retry
 
+
+# cause the current thread to sleep until the specified time period has ellapsed
+# and then retry the function
 @sleep_and_retry
 @limits(calls=1, period=1)
 def call_api():

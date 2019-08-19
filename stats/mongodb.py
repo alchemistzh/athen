c9fc6b59629ece597afc1dfdb74f567c08ba06ac
zh <zh@xrc-inc.com>
2019-08-19 17:38:56 +0800

    Group samples

---
+++ stats/mongodb.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import pymongo
+
+
+URI = 'mongodb://localhost:27017'
+DB_ATHEN = 'athen'
+db = pymongo.MongoClient(URI)[DB_ATHEN]
+
+col_stock_profile = db['stock_profile']
+col_shareholder = db['shareholder']
+col_subject = db['subject']

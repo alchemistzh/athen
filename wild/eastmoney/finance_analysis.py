fedc482b437a85df9eb4f07a345b7b44037d3e20
zh <zh@xrc-inc.com>
2019-08-19 17:24:39 +0800

    Test eastmoney finance analysis api

--- wild/eastmoney/finance_analysis.py
+++ wild/eastmoney/finance_analysis.py
@@ -8,16 +8,29 @@
     见 samples/finance_analysis.json
 """
 
+import requests
+
+
 url = 'http://f10.eastmoney.com/NewFinanceAnalysis/MainTargetAjax'
 
 params = {
     'type': '1',  # 0： 按报告期  1: 按年度
-    'code': 'SZ300496'
+    'code': 'SZ300753'
 }
 
-heasers = {
+headers = {
     'Accept': '*/*',
-    'Referer': 'http://f10.eastmoney.com/NewFinanceAnalysis/Index?type=web&code=SZ300496',
+    'Referer': 'http://f10.eastmoney.com/NewFinanceAnalysis/Index?type=web&code=SZ300753',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
     'X-Requested-With': 'XMLHttpRequest'
 }
+
+
+def get_finance_analysis(stock_code):
+    resp = requests.get(url, headers=headers, params=params)
+    data = resp.json()
+    print(data)
+    return data
+
+
+get_finance_analysis('')

31acd11b297f980172af7ce434911998cb1d68e9
zh <zh@xrc-inc.com>
2019-08-29 15:52:14 +0800

    [WIP]Sync finance indicator by report date

---
+++ sync/finance_indicator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import logging
+import requests
+import time
+from datetime import datetime
+
+import pymongo
+
+from wild.xueqiu.finance_indicator import get_finance_indicator
+from .mongodb import col_stock_profile, col_finance
+
+
+def pull_and_save(code):
+    fi_data = get_finance_indicator(code, 10)
+    for fi in fi_data['list']:
+        doc = dict(
+            report_name=fi['report_name'],
+            report_date=datetime.fromtimestamp(int(fi['report_date'])/1000).date(),
+            update_time=datetime.now()
+        )
+        col_finance.update_one(
+            {'_id': code},
+            {'$set': doc},
+            upsert=True
+        )
+
+
+stock_profile_docs = col_stock_profile.find(projection=[])
+for d in stock_profile_docs:
+    try:
+        pull_and_save(d['_id'])
+    except Exception as e:
+        logging.warning(d['_id'], e)
+        continue

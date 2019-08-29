4b3b94a514e3a3a8e63e91665759db852be5ba7d
zh <zh@xrc-inc.com>
2019-08-29 15:46:49 +0800

    Xueqiu finance indicator API

--- wild/xueqiu/cookie.py
+++ wild/xueqiu/cookie.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+"""
+调用雪球 API 之前需要先获取 cookie
+"""
 
-def get_cookies(session):
+import requests
+
+
+cookies = None
+
+
+def get_cookies(session=None):
     """
-    访问雪球首页, 获取 cookie
+    访问雪球首页, 获取 cookie 并保存
     """
+    global cookies
+    if cookies:
+        return cookies
     url = 'https://xueqiu.com'
     headers = {
         'Accept': '*/*',
@@ -16,5 +28,9 @@ def get_cookies(session):
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
     }
-    resp = session.get(url, headers=headers)
-    return resp.cookies
+    if session:
+        resp = session.get(url, headers=headers)
+    else:
+        resp = requests.get(url, headers=headers)
+    cookies = resp.cookies
+    return cookies

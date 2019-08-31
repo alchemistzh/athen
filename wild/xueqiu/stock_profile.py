5cae882cb0830dcc04e19fbd3c191627e7b98ec3
zh <zh@xrc-inc.com>
2019-08-31 13:05:19 +0800

    Cookies

--- wild/xueqiu/stock_profile.py
+++ wild/xueqiu/stock_profile.py
@@ -10,21 +10,18 @@
 
 import requests
 import sys
-from wild.xueqiu.cookie import get_cookies
+
 from wild.util import parse_percent, str_to_int
+from wild.xueqiu.cookie import get_cookies
 
 
 session = requests.Session()
-cookies = None
 
 
 def get_stock_profile(stock_code):
     """
     stock_code -- 6 位股票代码
     """
-    global cookies
-    if cookies is None:
-        cookies = get_cookies(session)
     code = '{}{}'.format('SH' if stock_code.startswith('6') else 'SZ', stock_code)
     url = 'https://stock.xueqiu.com/v5/stock/quote.json'
     params = {
@@ -41,7 +38,7 @@ def get_stock_profile(stock_code):
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
     }
-    resp = session.get(url, params=params, headers=headers, cookies=cookies)
+    resp = session.get(url, params=params, headers=headers, cookies=get_cookies())
     return resp.json()['data']['quote']
 
 

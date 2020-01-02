c1d152230ccf2667110528d78c5c1dd8399fada2
bluecover <gurity@163.com>
2020-01-02 23:38:00 +0800

    update lastest report date

--- util/datetime.py
+++ util/datetime.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-CURRENT_REPORT_DATE = '2019-06-30'
+CURRENT_REPORT_DATE = '2019-09-30'
 
 REPORT_DATES = [
+    '2019-09-30',
     '2019-06-30',
     '2019-03-31',
     '2018-12-31'
